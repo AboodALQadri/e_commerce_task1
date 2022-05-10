12726caafb0f9c71c69329e173b4ee988afb168d
AboodALQadri <bopy-15@hotmail.com>
2022-05-10 19:39:38 +0300

    commit - Payment 3

--- lib/logic/bindings/main_binding.dart
+++ lib/logic/bindings/main_binding.dart
@@ -1,4 +1,5 @@
 import 'package:e_commerce_task1/logic/controllers/main_controller.dart';
+import 'package:e_commerce_task1/logic/controllers/payment_controller.dart';
 import 'package:e_commerce_task1/logic/controllers/settings_controller.dart';
 import 'package:get/get.dart';
 
@@ -7,5 +8,6 @@ class MainBinding implements Bindings {
   void dependencies() {
     Get.put(MainController());
     Get.put(SettingsController());
+    Get.put(PaymentController());
   }
 }
--- lib/view/widgets/payment/delivery_container_widget.dart
+++ lib/view/widgets/payment/delivery_container_widget.dart
@@ -1,3 +1,5 @@
+import 'package:e_commerce_task1/logic/controllers/payment_controller.dart';
+import 'package:e_commerce_task1/routes/routes.dart';
 import 'package:e_commerce_task1/utils/theme.dart';
 import 'package:e_commerce_task1/view/widgets/text_utils.dart';
 import 'package:flutter/material.dart';
@@ -12,8 +14,12 @@ class DeliveryContainerWidget extends StatefulWidget {
 }
 
 class _DeliveryContainerWidgetState extends State<DeliveryContainerWidget> {
+  final TextEditingController _phoneController = TextEditingController();
+
   int radioContainerIndex = 1;
   bool changeColor = false;
+
+  final _controller = Get.find<PaymentController>();
 
   @override
   Widget build(BuildContext context) {
@@ -22,8 +28,9 @@ class _DeliveryContainerWidgetState extends State<DeliveryContainerWidget> {
         buildRadioContainer(
           title: 'Uzay Shop',
           name: 'Uzay store',
-          phone: '059-1234-567',
+          phone: '59-876-5432',
           address: 'Gaza , AL-Remal Street',
+          icon: Container(),
           value: 1,
           color: changeColor ? Colors.white : Colors.grey.shade400,
           onChanged: (int? value) {
@@ -36,20 +43,108 @@ class _DeliveryContainerWidgetState extends State<DeliveryContainerWidget> {
         const SizedBox(
           height: 10,
         ),
-        buildRadioContainer(
-          title: 'Delivery',
-          name: 'Abood AL-Qadri',
-          phone: '056-707-4004',
-          address: 'Gaza , AL-Remal Street',
-          value: 2,
-          color: changeColor ? Colors.grey.shade400 : Colors.white,
-          onChanged: (int? value) {
-            setState(() {
-              radioContainerIndex = value!;
-              changeColor = !changeColor;
-            });
+        Obx(
+          () {
+            return buildRadioContainer(
+              title: 'Delivery',
+              name: 'Abood AL-Qadri',
+              phone: _controller.phoneNumber.value,
+              address: 'Gaza , AL-Remal Street',
+              value: 2,
+              icon: InkWell(
+                onTap: () {
+                  Get.defaultDialog(
+                    title: "Enter Your Phone Number",
+                    titleStyle: const TextStyle(
+                      fontSize: 16,
+                      color: Colors.black,
+                      fontWeight: FontWeight.bold,
+                    ),
+                    backgroundColor: Colors.white,
+                    radius: 10,
+                    //
+                    textCancel: " Cancel ",
+                    cancelTextColor: Colors.black,
+                    onCancel: () {
+                      Get.toNamed(Routes.paymentScreen);
+                    },
+                    //
+                    textConfirm: " Save ",
+                    confirmTextColor: Colors.black,
+                    onConfirm: () {
+                      Get.back();
+                      _controller.phoneNumber.value = _phoneController.text;
+                    },
+                    buttonColor: Get.isDarkMode ? pinkClr : mainColor,
+                    content: Padding(
+                      padding: const EdgeInsets.all(15),
+                      child: TextField(
+                        controller: _phoneController,
+                        cursorColor: Colors.black,
+                        maxLength: 9,
+                        keyboardType: TextInputType.text,
+                        onSubmitted: (value) {
+                          _phoneController.text = value;
+                        },
+                        decoration: InputDecoration(
+                          fillColor: Get.isDarkMode
+                              ? pinkClr.withOpacity(0.2)
+                              : mainColor.withOpacity(0.2),
+                          focusColor: Colors.red,
+                          prefixIcon: Icon(
+                            Icons.phone,
+                            color: Get.isDarkMode ? pinkClr : mainColor,
+                          ),
+                          suffixIcon: IconButton(
+                            onPressed: () {
+                              _phoneController.clear();
+                            },
+                            icon: const Icon(Icons.close, color: Colors.black),
+                          ),
+                          hintText: "Enter Your Phone Number",
+                          hintStyle: const TextStyle(
+                            color: Colors.black45,
+                            fontSize: 12,
+                            fontWeight: FontWeight.w500,
+                          ),
+                          filled: true,
+                          enabledBorder: OutlineInputBorder(
+                            borderSide: const BorderSide(color: Colors.white),
+                            borderRadius: BorderRadius.circular(10),
+                          ),
+                          focusedBorder: OutlineInputBorder(
+                            borderSide: const BorderSide(color: Colors.white),
+                            borderRadius: BorderRadius.circular(10),
+                          ),
+                          errorBorder: OutlineInputBorder(
+                            borderSide: const BorderSide(color: Colors.white),
+                            borderRadius: BorderRadius.circular(10),
+                          ),
+                          focusedErrorBorder: OutlineInputBorder(
+                            borderSide: const BorderSide(color: Colors.white),
+                            borderRadius: BorderRadius.circular(10),
+                          ),
+                        ),
+                      ),
+                    ),
+                  );
+                },
+                child: Icon(
+                  Icons.contact_phone,
+                  color: Get.isDarkMode ? pinkClr : mainColor,
+                  size: 20,
+                ),
+              ),
+              color: changeColor ? Colors.grey.shade400 : Colors.white,
+              onChanged: (int? value) {
+                setState(() {
+                  radioContainerIndex = value!;
+                  changeColor = !changeColor;
+                });
+              },
+            );
           },
-        ),
+        )
       ],
     );
   }
@@ -62,6 +157,7 @@ class _DeliveryContainerWidgetState extends State<DeliveryContainerWidget> {
     required String name,
     required String phone,
     required String address,
+    required Widget icon,
   }) {
     return Container(
       height: 120,
@@ -87,7 +183,8 @@ class _DeliveryContainerWidgetState extends State<DeliveryContainerWidget> {
             onChanged: (int? value) {
               onChanged(value);
             },
-            fillColor: MaterialStateColor.resolveWith((states) => Get.isDarkMode ? pinkClr : mainColor),
+            fillColor: MaterialStateColor.resolveWith(
+                (states) => Get.isDarkMode ? pinkClr : mainColor),
           ),
           const SizedBox(
             width: 10,
@@ -117,12 +214,28 @@ class _DeliveryContainerWidgetState extends State<DeliveryContainerWidget> {
                 const SizedBox(
                   height: 5,
                 ),
-                TextUtils(
-                  text: phone,
-                  color: Colors.black,
-                  fontWeight: FontWeight.normal,
-                  fontSize: 15,
-                  underLine: TextDecoration.none,
+                Row(
+                  children: [
+                    Text(
+                      '🇵🇸+970 ',
+                      style: TextStyle(
+                        color: Get.isDarkMode ? Colors.white : Colors.black,
+                      ),
+                    ),
+                    TextUtils(
+                      text: phone,
+                      color: Colors.black,
+                      fontWeight: FontWeight.normal,
+                      fontSize: 15,
+                      underLine: TextDecoration.none,
+                    ),
+                    SizedBox(
+                      width: 120,
+                    ),
+                    SizedBox(
+                      child: icon,
+                    ),
+                  ],
                 ),
                 const SizedBox(
                   height: 5,
